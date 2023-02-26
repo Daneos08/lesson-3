f63e3aad6417fb6ac423c39c014c3c63e2e5244e
Daneos08 <113821056+Daneos08@users.noreply.github.com>
2023-02-26 07:21:42 +0000

    add Clas, Me and You.

---
+++ main.py
@@ -0,0 +1,10 @@
+class Person:
+  height = 170
+  name = "Name"
+  is_sad = True
+  def __init__(self, name , height):
+    self.name = name
+    self.height = height
+
+me = Person('Denis', 179)
+you = Person('Name', 175)
